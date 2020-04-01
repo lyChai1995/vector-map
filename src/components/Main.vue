bb0b7b57eff7bf471a7e4b93c4a9a1e743869baf
lychai <pgdog1@163.com>
2020-04-01 14:34:18 +0800

    12

---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="vectorMap-test">
+      <VectorMap></VectorMap>
+    </div>
+  </div>
+</template>
+
+<script>
+import VectorMap from "./VectorMap";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    VectorMap
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.container {
+  padding: 20px;
+  .vectorMap-test {
+    width: 700px;
+    height: 500px;
+  }
+}
+</style>
